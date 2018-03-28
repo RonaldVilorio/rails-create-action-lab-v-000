0199429fb61edecc316d80124d250afbbc5e6fe1
ronald vilorio <soccer23415@gmail.com>
2018-03-28 01:04:22 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,7 +16,7 @@ class StudentsController < ApplicationController
     @student = Student.new
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
-    redirect_to student_path(@student)
+    redirect_to students_path(@student)
 
   end
 
